d0c245aeec2ced37acdaa0e38eafbe5c0da0de2b
Maralmhz <maralmhz@gmail.com>
2026-09-08 09:15:19 -0300

    Update iframe source URL for eveUpdate eventos iframe URL to eventosportomaisnt panel

--- app/eventos/page.js
+++ app/eventos/page.js
@@ -8,8 +8,7 @@ export default function EventosPage() {
       <div style={{ display: 'flex', flexDirection: 'column', height: '100vh' }}>
         <MenuPrincipal />
         <iframe
-          src="https://painel-eventos.vercel.app/"
-          title="Painel de Eventos"
+        src="https://eventosportomais.vercel.app/"          title="Painel de Eventos"
           style={{ flex: 1, border: 'none', width: '100%' }}
         />
       </div>
